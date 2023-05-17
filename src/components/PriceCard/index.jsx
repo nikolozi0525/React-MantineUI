af0676e1f6aece4856b56431e2332fcbb3aa3f7e
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-17 16:29:53 -0400

    Fix up font on PriceCard

--- src/components/PriceCard/index.jsx
+++ src/components/PriceCard/index.jsx
@@ -16,34 +16,43 @@ const PriceCard = (props) => {
   const useStyles = createStyles((theme) => ({
     container: {
       width: "100%",
-      background: props.theme === "black" ? "black" : "white",
+      background: props.theme === "black" ? "#232323" : "white",
+      fontFamily: "Inter",
+      padding: 30,
     },
     startBtn: {
-      fontSize: rem(16),
+      fontSize: rem(18),
       marginTop: 20,
-      marginBottom: 10,
+      marginBottom: 20,
+      fontFamily: "Inter",
     },
     typedesc: {
       fontSize: rem(20),
+      fontFamily: "Inter",
       color: props.theme === "black" ? "white" : "black",
     },
     type: {
       marginBottom: 10,
     },
     p10: {
-      marginTop: 10,
+      marginTop: 20,
       marginBottom: 10,
       opacity: "90%",
       color: props.theme === "black" ? "white" : "#979797",
       fontSize: rem(18),
+      fontFamily: "Inter",
     },
     price: {
       fontSize: rem(36),
+      fontFamily: "Inter",
       fontWeight: 600,
       color: props.theme === "black" ? "white" : "black",
     },
     checkgroup: {
       marginTop: 46,
+    },
+    myfont: {
+      fontFamily: "Inter",
     },
   }));
   const { classes } = useStyles();
@@ -51,7 +60,7 @@ const PriceCard = (props) => {
     <div>
       <Card
         shadow="sm"
-        radius={"md"}
+        radius={"lg"}
         withBorder
         className={classes.container}
         padding={30}
@@ -67,22 +76,24 @@ const PriceCard = (props) => {
         <div className={classes.checkgroup}>
           <Group className={classes.p10}>
             <img src="assets/images/check.png" alt="" />
-            <Text>{staff} Staff Members</Text>
+            <Text className={classes.myfont}>{staff} Staff Members</Text>
           </Group>
           <Group className={classes.p10}>
             <img src="assets/images/check.png" alt="" />
-            <Text>{patient} Patients</Text>
+            <Text className={classes.myfont}>{patient} Patients</Text>
           </Group>
           <Group className={classes.p10}>
             <img src="assets/images/check.png" alt="" />
-            <Text>{daily} Daily Updates</Text>
+            <Text className={classes.myfont}>{daily} Daily Updates</Text>
           </Group>
         </div>
-        <Center>
-          <Button color={"yellow"} className={classes.startBtn} size="md">
-            Get started
-          </Button>
-        </Center>
+        <Card.Section>
+          <Center>
+            <Button color={"yellow"} className={classes.startBtn} size="md">
+              Get started
+            </Button>
+          </Center>
+        </Card.Section>
       </Card>
     </div>
   );
