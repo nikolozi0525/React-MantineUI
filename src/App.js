20e6aa29de3a8b37fd0fd20d0a5833c4a5dab8ba
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-17 15:44:24 -0400

    Add links on app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import BusinessProfile from "./pages/BusinessProfile";
 import LandingPage from "./layouts/landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
-import "./App.css";
+import SimplePricing from "./pages/SimplePricing";
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<BusinessProfile />} />
+          <Route path="/selectprice" element={<SimplePricing />} />
         </Routes>
       </BrowserRouter>
     </div>
