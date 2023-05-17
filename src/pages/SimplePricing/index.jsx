bb52b0632ee241978396586fa84eb7f88dca5aec
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-17 16:30:51 -0400

    Create Simple Pricing with style

--- src/pages/SimplePricing/index.jsx
+++ src/pages/SimplePricing/index.jsx
@@ -1,4 +1,4 @@
-import { Grid, SimpleGrid } from "@mantine/core";
+import { Container, Grid, SimpleGrid, Text } from "@mantine/core";
 
 import PriceCard from "../../components/PriceCard";
 import useStyles from "./style";
@@ -7,30 +7,37 @@ const SimplePricing = () => {
   const { classes } = useStyles();
   return (
     <>
-      <div>
-        <div className={classes.cardcontainer}>
-          <SimpleGrid cols={3} spacing={50}>
-            <PriceCard
-              staff={0}
-              patient={2}
-              daily={5}
-              src="assets/images/trial.png"
-            />
-            <PriceCard
-              staff={4}
-              patient={5}
-              daily={"Unlimited"}
-              theme="black"
-              src="assets/images/personal.png"
-            />
-            <PriceCard
-              staff={"Unlimited"}
-              patient={"Unlimited"}
-              daily={"Unlimited"}
-              src="assets/images/agency.png"
-            />
-          </SimpleGrid>
+      <Container className={classes.centered}>
+        <div className={classes.logo}>
+          <img src="assets/images/logo2.png" alt="" />
         </div>
+        <Text className={classes.title}>Simple Pricing</Text>
+        <Text className={classes.description}>
+          Select a package that suits you
+        </Text>
+      </Container>
+      <div className={classes.cardcontainer}>
+        <SimpleGrid cols={3} spacing={50}>
+          <PriceCard
+            staff={0}
+            patient={2}
+            daily={5}
+            src="assets/images/trial.png"
+          />
+          <PriceCard
+            staff={4}
+            patient={5}
+            daily={"Unlimited"}
+            theme="black"
+            src="assets/images/personal.png"
+          />
+          <PriceCard
+            staff={"Unlimited"}
+            patient={"Unlimited"}
+            daily={"Unlimited"}
+            src="assets/images/agency.png"
+          />
+        </SimpleGrid>
       </div>
     </>
   );
--- src/pages/SimplePricing/style.js
+++ src/pages/SimplePricing/style.js
@@ -1,9 +1,30 @@
-import { createStyles } from "@mantine/core";
+import { createStyles, rem } from "@mantine/core";
 
 const useStyles = createStyles((theme) => ({
   cardcontainer: {
-    width: "60%",
+    width: "57%",
     margin: "auto",
+  },
+  centered: {
+    alignItems: "center",
+    textAlign: "center",
+  },
+  title: {
+    fontSize: rem(34),
+    fontWeight: 700,
+    fontFamily: "Inter",
+    marginBottom: rem(20),
+  },
+  description: {
+    fontSize: rem(20),
+    fontWeight: 400,
+    fontFamily: "Inter",
+    color: "#979797",
+    marginBottom: rem(50),
+  },
+  logo: {
+    marginTop: rem(80),
+    marginBottom: rem(111),
   },
 }));
 
