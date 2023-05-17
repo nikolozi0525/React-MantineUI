20cb4ac29d4c44a8205287a17a8359ed7169c5be
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-16 22:02:21 -0400

    Create Signup with style

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,5 +1,127 @@
+import {
+  Anchor,
+  Button,
+  Checkbox,
+  Grid,
+  Group,
+  PasswordInput,
+  Text,
+  TextInput,
+} from "@mantine/core";
+
+import { IconBrandGoogle } from "@tabler/icons-react";
+import axios from "axios";
+import { useForm } from "@mantine/form";
+import useStyles from "./style";
+
 const Signup = () => {
-  return <div className="sign-up">Signup page</div>;
+  const { classes } = useStyles();
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      repassword: "",
+      rememberme: false,
+    },
+  });
+  const signup = () => {
+    alert(form.values.name);
+    // axios.post('localhost:3000')
+  };
+  return (
+    <Grid>
+      <Grid.Col span={6} className={classes.left_pane}>
+        <div className={classes.welcome}>
+          <img
+            className={classes.logo_img}
+            src="assets/images/logo.png"
+            alt=""
+          />
+          <Text fs={"italic"} fw={700} sx={{ fontFamily: "Inter" }} fz={38}>
+            Welcome Back
+          </Text>
+          <Text c="white">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </Text>
+        </div>
+
+        <div>
+          <img
+            className={classes.glass_man}
+            src="assets/images/beanbag.png"
+            alt=""
+          />
+        </div>
+        <img
+          className={classes.credential}
+          src="assets/images/credential.png"
+          alt=""
+        />
+      </Grid.Col>
+      <Grid.Col span={6}>
+        <div className={classes.toptitle}>
+          <Text>
+            Already have an account?
+            <Anchor className={classes.signup}>Login</Anchor>
+          </Text>
+        </div>
+        <div className={classes.myform}>
+          <Text className={classes.title}>Sign up</Text>
+          <form onSubmit={form.onSubmit((values) => console.log)}>
+            <TextInput
+              label="Name"
+              width={100}
+              placeholder="EG.abc"
+              {...form.getInputProps("name")}
+            />
+            <TextInput
+              label="Email"
+              width={100}
+              placeholder="EG.abc@gmail.com"
+              {...form.getInputProps("email")}
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Eg.A*****"
+              {...form.getInputProps("password")}
+            />
+            <PasswordInput
+              label="Re-enter Password"
+              placeholder="Eg.A*****"
+              {...form.getInputProps("repassword")}
+            />
+            <div className={classes.rememberbox}>
+              <Group>
+                <Checkbox
+                  label={"I agree with the Terms of Service and Privacy policy"}
+                  className={classes.rememberme}
+                />
+              </Group>
+            </div>
+            <Button
+              color="yellow"
+              className={classes.login_btn}
+              onClick={signup}
+            >
+              Sign up
+            </Button>
+            <div>
+              <Button
+                variant="subtle"
+                leftIcon={<IconBrandGoogle />}
+                className={classes.login_btn_google}
+              >
+                Login with the Google
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Grid.Col>
+    </Grid>
+  );
 };
 
 export default Signup;
---
+++ src/pages/Signup/style.js
@@ -0,0 +1,82 @@
+import { createStyles, em, rem } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  left_pane: {
+    width: "50%",
+    height: "100vh",
+    background: "#FFBA45",
+  },
+  welcome: {
+    position: "relative",
+    top: "5.5%",
+    left: "5.5%",
+    maxWidth: "80%",
+  },
+  logo_img: {
+    width: "11.57%",
+    aspectRatio: 2.7,
+  },
+  glass_man: {
+    marginTop: "9.4%",
+    marginLeft: "8.95%",
+    width: "59.2%",
+    aspectRatio: 1.298,
+  },
+  credential: {
+    position: "absolute",
+    top: "24%",
+    left: "26.09%",
+    width: "19%",
+    aspectRatio: "0.8693",
+  },
+  myform: {
+    maxWidth: "70%",
+    margin: "auto",
+    position: "relative",
+    top: "10%",
+  },
+  rememberbox: {
+    marginTop: rem(15),
+    marginBottom: rem(15),
+  },
+  forgot: {
+    marginLeft: "auto",
+    color: "#FFBA45",
+    textDecoration: "underline",
+    fontSize: rem(12),
+  },
+  signup: {
+    color: "#FFBA45",
+    textDecoration: "underline",
+  },
+  toptitle: {
+    position: "relative",
+    top: "5.5%",
+    right: "5.5%",
+    fontSize: rem(14),
+    textAlign: "right",
+  },
+  title: {
+    marginTop: rem(12),
+    marginBottom: rem(12),
+    fontSize: rem(32),
+    fontWeight: 700,
+    fontFamily: "Inter",
+  },
+  login_btn: {
+    "@media (max-width :80em)": {
+      width: "100%",
+    },
+    width: "50%",
+  },
+  login_btn_google: {
+    "@media (max-width :80em)": {
+      width: "100%",
+    },
+    width: "50%",
+    marginTop: rem(20),
+    color: "grey",
+  },
+}));
+
+export default useStyles;
