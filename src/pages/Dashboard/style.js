6204b6143391924db4818a28ab8c49d66da0ab02
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-18 16:49:18 -0400

    Create Dashboard page

---
+++ src/pages/Dashboard/style.js
@@ -0,0 +1,14 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    display: "flex",
+  },
+  content: {
+    display: "flex",
+    width: "100%",
+    flexDirection: "column",
+  },
+}));
+
+export default useStyles;
