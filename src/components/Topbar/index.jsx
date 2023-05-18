6765e51d7733a8f0b95345b644c4f45c46cb6045
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-18 16:47:45 -0400

    Create Topbar component

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -6,35 +6,33 @@ import useStyles from "./style";
 const Topbar = () => {
   const { classes } = useStyles();
   return (
-    <>
-      <Container className={classes.wrapper} fluid>
-        <Text className={classes.myfont}>Dashboard</Text>
-        <ActionIcon
-          variant="light"
-          size={52}
-          className={classes.float_right}
-          mx={5}
-        >
-          <IconSettings size={40} color="#232323" stroke={1} />
-        </ActionIcon>
-        <ActionIcon
-          variant="light"
-          size={52}
-          className={classes.float_right}
-          mx={5}
-        >
-          <IconBell size={40} color="#232323" stroke={1} />
-        </ActionIcon>
-        <TextInput
-          placeholder="Search something..."
-          sx={{ fontFamily: "Inter", width: 514 }}
-          icon={<IconSearch />}
-          size="lg"
-          radius={12}
-          className={classes.float_right}
-        />
-      </Container>
-    </>
+    <Container className={classes.wrapper} fluid p={0}>
+      <Text className={classes.myfont}>Dashboard</Text>
+      <ActionIcon
+        variant="light"
+        size={52}
+        className={classes.float_right}
+        mx={5}
+      >
+        <IconSettings size={40} color="#232323" stroke={1} />
+      </ActionIcon>
+      <ActionIcon
+        variant="light"
+        size={52}
+        className={classes.float_right}
+        mx={5}
+      >
+        <IconBell size={40} color="#232323" stroke={1} />
+      </ActionIcon>
+      <TextInput
+        placeholder="Search something..."
+        sx={{ fontFamily: "Inter", width: 514 }}
+        icon={<IconSearch />}
+        size="lg"
+        radius={12}
+        className={classes.float_right}
+      />
+    </Container>
   );
 };
 
