7f9453408b69b790e2ee31b4d2248da8026da1ca
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-19 16:36:16 -0400

    Add modal on facility page

--- src/pages/Facility/index.jsx
+++ src/pages/Facility/index.jsx
@@ -1,9 +1,21 @@
-import { Container, createStyles } from "@mantine/core";
+import {
+  Button,
+  Container,
+  Group,
+  Modal,
+  Select,
+  SimpleGrid,
+  Text,
+  TextInput,
+  createStyles,
+} from "@mantine/core";
 
 import AddButton from "../../components/AddButton";
+import FacilityModal from "../../components/FacilityModal";
 import FacilityTable from "../../components/FacilityTable";
 import Nav from "../../components/Navbar";
 import Topbar from "../../components/Topbar";
+import { useDisclosure } from "@mantine/hooks";
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
@@ -18,20 +30,34 @@ const useStyles = createStyles((theme) => ({
     marginLeft: "auto",
     marginTop: 50,
   },
+  mymodal: {
+    width: 818,
+  },
+  mwd: {
+    width: "100%",
+  },
 }));
 const Facility = (props) => {
   const { classes } = useStyles();
+  const [opened, { open, close }] = useDisclosure(false);
   return (
-    <Container className={classes.wrapper} fluid p={0}>
-      <Nav />
-      <Container fluid className={classes.content} px={40}>
-        <Topbar title={"Facility Management"} />
-        <div className={classes.btn}>
-          <AddButton title={"Add New Facility"} />
-        </div>
-        <FacilityTable />
+    <>
+      <Container className={classes.wrapper} fluid p={0}>
+        <Nav />
+        <Container fluid className={classes.content} px={40}>
+          <FacilityModal
+            opened={opened}
+            close={close}
+            title={"Add New Facility"}
+          />
+          <Topbar title={"Facility Management"} />
+          <div className={classes.btn}>
+            <AddButton title={"Add New Facility"} onClick={open} />
+          </div>
+          <FacilityTable />
+        </Container>
       </Container>
-    </Container>
+    </>
   );
 };
 
