fe7126ce61669e967f9e9b72d5a1ee4eda417bb0
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-20 16:04:10 -0400

    create staffTabelRow component

---
+++ src/components/StaffTableRow/index.jsx
@@ -0,0 +1,81 @@
+import {
+  ActionIcon,
+  Center,
+  Group,
+  SimpleGrid,
+  Switch,
+  Text,
+  createStyles,
+  rem,
+} from "@mantine/core";
+import { IconEdit, IconKey, IconTrash } from "@tabler/icons-react";
+
+import StaffModal from "../StaffModal";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    borderWidth: 1,
+    boderColor: "#F2F2F2",
+    borderStyle: "solid",
+    borderRadius: 12,
+    width: "100%",
+    paddingTop: rem(12),
+    paddingBottom: rem(12),
+    // paddingLeft: rem(50),
+    fontSize: 16,
+    fontFamily: "Inter",
+    marginTop: 15,
+    color: "#A8A8A8",
+  },
+  cell: {
+    paddingLeft: rem(50),
+    fontFamily: "Inter",
+  },
+}));
+const StaffTableRow = ({ row }) => {
+  const { classes } = useStyles();
+  const [active, setActive] = useState(row.active);
+  const [opened, { open, close }] = useDisclosure(false);
+  return (
+    <SimpleGrid cols={6} className={classes.wrapper}>
+      <StaffModal
+        opened={opened}
+        onclose={close}
+        title={"Edit Facility"}
+        data={row}
+      />
+      <Text className={classes.cell}>{row.name}</Text>
+      <Text className={classes.cell}>{row.jobtitle}</Text>
+      <Text className={classes.cell}>{row.email}</Text>
+      <Text sx={{ margin: "auto" }}>
+        <Switch
+          size="lg"
+          color="yellow"
+          checked={active}
+          onChange={() => {
+            setActive(!active);
+          }}
+        />
+      </Text>
+      <Text sx={{ margin: "auto" }}>
+        <ActionIcon variant="light" onClick={open}>
+          <IconKey />
+        </ActionIcon>
+      </Text>
+      <Text sx={{ margin: "auto" }}>
+        <Group>
+          <ActionIcon color="green" variant="light" onClick={open}>
+            <IconEdit />
+          </ActionIcon>
+          <ActionIcon color="red" variant="light">
+            <IconTrash />
+          </ActionIcon>
+        </Group>
+      </Text>
+    </SimpleGrid>
+  );
+};
+
+export default StaffTableRow;
