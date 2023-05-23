6ac697ea8644d08dcbfbd00f0787912c481c7be0
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-05-23 11:28:03 -0400

    Fixup navbar size

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -88,7 +88,7 @@ export default function Nav() {
   // ));
 
   return (
-    <Navbar height={1080} width={{ sm: 382 }} p={40}>
+    <Navbar height={1000} width={{ sm: 382 }} p={40}>
       <Navbar.Section grow>
         <Center className={classes.header}>
           <img src="/assets/images/logo3.png" alt="Logo" />
