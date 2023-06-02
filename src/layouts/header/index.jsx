fe8d8b66955c908395cb7edfc406f0bd160bc361
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-06-02 01:11:50 -0400

    Fixup header in landing page

--- src/layouts/header/index.jsx
+++ src/layouts/header/index.jsx
@@ -1,5 +1,69 @@
-const header = () => {
-  return <div className="header">this is header</div>;
+import {
+  Anchor,
+  Box,
+  Button,
+  Center,
+  Image,
+  SimpleGrid,
+  Text,
+  createStyles,
+  rem,
+} from "@mantine/core";
+
+import { Link } from "react-router-dom";
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  link: {
+    marginTop: rem(38),
+  },
+  linkgroup: {
+    marginTop: rem(38),
+    FontFamily: "Inter",
+  },
+  login: {
+    fontFamily: "Inter",
+    marginTop: rem(25),
+    marginRight: rem(100),
+  },
+}));
+const Header = () => {
+  const { classes } = useStyles();
+  return (
+    <Box className={classes.container}>
+      <Image
+        src="/assets/images/logo3.png"
+        width={"131"}
+        height={"48"}
+        mr={"auto"}
+        fit="fill"
+        mt={26}
+        ml={99}
+      />
+      <SimpleGrid
+        cols={3}
+        spacing={75}
+        mx={"auto"}
+        className={classes.linkgroup}
+      >
+        <Anchor>
+          <Center>Feature</Center>
+        </Anchor>
+        <Anchor>
+          <Center>Plan</Center>{" "}
+        </Anchor>
+        <Anchor>
+          <Center>Support</Center>{" "}
+        </Anchor>
+      </SimpleGrid>
+      <Button color="yellow" ml={"auto"} className={classes.login}>
+        Login
+      </Button>
+    </Box>
+  );
 };
 
-export default header;
+export default Header;
