8fbfd7875ce9c7ae48d87d749e2ae0a78d92379d
Nikolozi Svanadze <nikolozi0525@gmail.com>
2023-06-06 01:25:42 -0400

    Fixup Footer

--- src/layouts/footer/index.jsx
+++ src/layouts/footer/index.jsx
@@ -2,7 +2,7 @@ import { Center } from "@mantine/core";
 
 const footer = () => {
   return (
-    <Center bg="#282828" c="white" py="26px">
+    <Center bg="#282828" c="white" py="26px" ff={"Inter"}>
       <div className="footer">2023 Intuit Inc. All rights reserved.</div>
     </Center>
   );
